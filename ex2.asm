d97a5ec15909d7c2d19d13b72a0f5c444c15123b
master1auto_ouargla <microprocessor.m1auto@gmail.com>
2016-01-13 17:22:50 +0100

    Update ex2.asm

--- ex2.asm
+++ ex2.asm
@@ -1,1 +1,25 @@
+;configure all ports as output
+mov dx,606h
+mov al,80h
+out dx,al 
+;led PB3 is on
+cc:mov dx,602h
+mov al,08h
+out dx,al 
+call delay4s 
+;led PB3 is off
+mov al,00h
+out dx,al
+jmp cc
+hlt
 
+delay4s proc
+mov bx,1000
+ff:dec bx
+mov cx,20000
+tt:nop
+loop tt
+cmp bx,0
+jnz ff
+ret
+delay4s endp 
